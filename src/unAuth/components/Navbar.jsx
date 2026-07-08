af491c1307b84876474dd78f52b165b3c6536ee6
Osmani (Pititi) <osmanipititi@gmail.com>
2026-07-09 00:25:25 +0200

    We changed the navbar menu item Home to a Video Chatbot button

--- src/unAuth/components/Navbar.jsx
+++ src/unAuth/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { MonitorPlay } from 'phosphor-react'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">MyApp</div>
+      <div className="navbar-logo">
+        <MonitorPlay className="navbar-logo-icon" size={28} />
+        MyApp
+      </div>
       <ul className="navbar-links">
-        <li>Home</li>
+        <li><button className="nav-cta-btn">AI Video ChatBot →</button></li>
         <li>About</li>
         <li>Contact</li>
       </ul>
