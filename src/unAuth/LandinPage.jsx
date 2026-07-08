9ca07e83ff0d049407b226eb9e9e7435c5815533
Osmani (Pititi) <osmanipititi@gmail.com>
2026-07-08 15:14:07 +0200

    Landing page render

---
+++ src/unAuth/LandinPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LandingPage.css'
+
+const LandinPage = () => {
+  return (
+    <div className="landing-container">
+      <h1 className="landing-title">hello landing page</h1>
+    </div>
+  )
+}
+
+export default LandinPage
